4229132544c940e706e279a0ec30035a2bdfc9e4
Olgasaf <101572462+Olgasaf@users.noreply.github.com>
2022-05-29 19:48:49 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5342722255:AAGRbGMvapVvOuMEx6gax80NVQKqmSMJDmk'
+keys = {
+    'Юань': 'CNY',
+    'Йена': 'JPY',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Франк': 'CHF',
+    'Лира': 'TRY',
+    'Рубль': 'RUB',
+}
