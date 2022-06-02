435df817ad31f032939f90fb49daaa0a09cdb124
Olgasaf <101572462+Olgasaf@users.noreply.github.com>
2022-06-02 21:59:43 +0300

    Update add.py

--- add.py
+++ add.py
@@ -1,38 +1,55 @@
 import telebot
-from config import TOKEN, keys
-from Utils import ConvertionException, CryptoConverter
+
+from Utils import CryptoConverter, APIException
+from config import keys, TOKEN
+
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=["start"])
+def start(message: telebot.types.Message):
+    text = "Привет! Я RedDeerBot-котвертер валют и я могу:  "\
+    "\n- Показать список доступных валют через команду /values "\
+    "\n- Провести конвертацию валюты при Вашем вводе : <название вашей валюты> <в какую валюту перевести> <количество переводимой валюты>"\
+    "\n- Напомнить, что я могу через команду /help ."
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=["help"])
 def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу, введите команду в формате:\n<имя валюты>\
-<в какую валюту перевести>\<количество переводимой валюты>\nУвидеть список всех доступныых валют: /values'
+    text = "- Для конвертации введите Ваши данные в следующем формате: \n "\
+           " <название вашей валюты> <в какую валюту перевести> <количество переводимой валюты> ."
     bot.reply_to(message, text)
 
-@bot.message_handler(commands=['values'])
+@bot.message_handler(commands=["values"])
 def values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
+    text = "Список доступных валют: "
     for key in keys.keys():
-        text = '\n'.join((text, key))
+        text = "\n".join((text, key, ))
     bot.reply_to(message, text)
 
-@bot.message_handler(content_types=['text', ])
-def convert(message: telebot.types.Message):
+@bot.message_handler(content_types=["text"])
+def get_price(message: telebot.types.Message):
     try:
         values = message.text.split(' ')
-        if len(values) != 3:
-            raise ConvertionException('Слишком много параметров')
-        quote, base, amount = values
-        total_base = CryptoConverter.convert(quote, base, amount)
-    except ConvertionException as e:
-        bot.reply_to(message, f'Ошибка пользователя\n{e}')
 
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду\n(e')
+        if len(values) > 3:
+            raise APIException("Извините, слишком много данных.")
+        elif len(values) < 3:
+            raise APIException("Извините, слишком мало данных.")
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except APIException as e :
+        bot.reply_to(message, f"Внимание, Вы ошиблись\n{e}.")
+
+    except Exception as e :
+        bot.reply_to(message, f"Не удалось обработать команду\n{e}.")
+
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        x = float(amount)
+        text = f"Стоимость {amount} {quote} в {base} равна {total_base*x} ."
         bot.send_message(message.chat.id, text)
+
 
 bot.polling()
 
